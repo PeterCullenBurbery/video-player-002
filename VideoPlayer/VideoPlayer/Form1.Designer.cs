397daa89b7e15bb502c0667b17acdc20d79c82fe
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-09-26 12:13:50 -0400

    I created docked panels.

--- VideoPlayer/VideoPlayer/Form1.Designer.cs
+++ VideoPlayer/VideoPlayer/Form1.Designer.cs
@@ -28,18 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1717, 75);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 75);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1717, 696);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Bottom;
+            panel3.Location = new Point(0, 689);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1717, 82);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.Dock = DockStyle.Left;
+            panel4.Location = new Point(0, 75);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(97, 614);
+            panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.Dock = DockStyle.Right;
+            panel5.Location = new Point(1648, 75);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(69, 614);
+            panel5.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1717, 771);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
     }
 }
